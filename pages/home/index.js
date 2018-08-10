71b688768d118c7c049b2b34d629cee2e2f35f4f
tuda2008 <1756991323@qq.com>
2018-08-10 22:27:45 +0800

    update home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -16,6 +16,19 @@ Page({
         {'images': ['/images/home/carousel/factory1.jpg', '/images/home/carousel/factory2.jpg', 
         '/images/home/carousel/factory3.jpg'], 'id': 3, 'title': '产品c', 'intro': '产品描述c'}
         ],
+        customize: [
+        {'images': ['/images/home/carousel/factory1.jpg', '/images/home/carousel/factory2.jpg', 
+        '/images/home/carousel/factory3.jpg'], 'id': 1, 'title': '定制a', 'intro': '定制描述a'},
+        {'images': ['/images/home/carousel/factory1.jpg', '/images/home/carousel/factory2.jpg', 
+        '/images/home/carousel/factory3.jpg'], 'id': 2, 'title': '定制b', 'intro': '定制描述b'},
+        {'images': ['/images/home/carousel/factory1.jpg', '/images/home/carousel/factory2.jpg', 
+        '/images/home/carousel/factory3.jpg'], 'id': 3, 'title': '定制c', 'intro': '定制描述c'}
+        ],
+        partners: [
+        {'id': 1, 'title': '明德电子', 'web_url': ''},
+        {'id': 2, 'title': '高明光学', 'web_url': ''},
+        {'id': 3, 'title': '兴标龙表业', 'web_url': ''}
+        ],
         item_interval: 5000,
         item_duration: 0
     },
@@ -24,6 +37,9 @@ Page({
            url: "/pages/products/index/index",
         })
 	},
+    goToProduct:function(e){
+        console.log(e.currentTarget.dataset.id);
+    },
 	goToCustomize:function(){
 	    wx.switchTab({
 	      url: '/pages/customize/index/index',
@@ -33,5 +49,16 @@ Page({
 	    wx.navigateTo({
 	      url: '/pages/partners/index/index',
 	    });
-	}
+	},
+    callPhone:function(){
+        wx.makePhoneCall({
+          phoneNumber: '15014146191', //此号码并非真实电话号码，仅用于测试
+          success:function(){
+            console.log("拨打电话成功！")
+          },
+          fail:function(){
+            console.log("拨打电话失败！")
+          }
+        })
+    }
 })
